7a7c5fe8ba13b3bc895ddbecbe55232c42180d22
jaeyeoneej <5ikve@naver.com>
2022-10-20 01:48:30 +0900

    node_modules를 삭제

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from "react";
-import Button from "./components/Button";
+import TopButton from "./components/Feature/TopButton";
 import { __getTodos } from "./redux/modules/todosSlice";
 import GlobalStyle from "./Shared/GlobalStyle";
 import Router from "./Shared/Router";
 
 const App = () => {
-  // const printOne = () => {
-  //   console.log(1)
-  // }
-  // const printTwo = () => {
-  //   console.log(2)
-  // }
   return (
     <div>
       <GlobalStyle />
       <Router />
-      {/* <Button bgColor="red" color="white" width="200px" onClick={printOne}>버튼1</Button>
-      <Button onClick={printTwo}>버튼2</Button> */}
+      <TopButton />
     </div>
-    
   );
 };
 
