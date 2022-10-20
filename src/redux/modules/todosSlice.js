b6a807062e5e430f7ac18e461ebd962d63e322c7
jaeyeoneej <5ikve@naver.com>
2022-10-20 21:26:27 +0900

    키워드 - 로딩 인디케이터, 단위 #6
    변경사항 : 로딩 인디케이터 구현, 일부 폴더 재정렬

--- src/redux/modules/todosSlice.js
+++ src/redux/modules/todosSlice.js
@@ -11,12 +11,15 @@ const initialState = {
 export const __getTodos = createAsyncThunk(
   "todos/getTodos",
   async (payload, thunkAPI) => {
+
     try {
-      const data = await axios.get("https://shrouded-badlands-79466.herokuapp.com/todos");
+      const data = await axios.get("https://shrouded-badlands-79466.herokuapp.com/todos", { timeout: 30000 });
       return thunkAPI.fulfillWithValue(data.data);
+
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
+
   }
 );
 
@@ -58,8 +61,11 @@ export const todosSlice = createSlice({
       state.isLoading = true; // 네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
     },
     [__getTodos.fulfilled]: (state, action) => {
-      state.isLoading = false; // 네트워크 요청이 끝났으니, false로 변경합니다.
-      state.todos = action.payload; // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
+      state.isLoading = false
+      state.todos = action.payload;
+
+      // 네트워크 요청이 끝났으니, false로 변경합니다.
+      // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
     },
     [__getTodos.rejected]: (state, action) => {
       state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
@@ -90,9 +96,9 @@ export const todosSlice = createSlice({
       state.isLoading = false;
       state.error = action.payload;
     },
-    
+
   },
 });
 
-export const {} = todosSlice.actions;
+export const { } = todosSlice.actions;
 export default todosSlice.reducer;
